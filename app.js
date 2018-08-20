b685950b3f7ced394597eed22a4791b2101747cb
Juan <juan.chavez@autozone.com>
2018-08-20 09:49:52 -0600

    Learning Angular: ng-model 2

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var app = angular.module('DemoApp',[]);
-	app.controller('DemoCtrl', function($scope){
-		$scope.pDescription="This topic looks at how Angular JS works \nModels in Angular JS"});
+var app = angular.module('DemoApp', []);
+app.controller('DemoCtrl', function($scope) {
+  $scope.pname = "Juan";
+
+  $scope.Topic = {
+    Controller: true,
+    Models: false
+  };
+});
