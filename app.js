b1af44ccfe093c4f2de3ba4978aad45724986b51
Juan <juan.chavez@autozone.com>
2018-08-20 12:48:19 -0600

    Learning Angular: ngRoute

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var app = angular.module('DemoApp',[]);
-	app.controller('DemoCtrl', function($scope){
-	$scope.pName="Guru99";
+var app = angular.module('sampleApp',["ngRoute"]);
+    app.config(function($routeProvider){
+        $routeProvider.
+        when("/NewEvent",{
+            templateUrl : "add_event.html",
+            controller: "AddEventController"
+        }).
+        when("/DisplayEvent", {
+            templateUrl: "show_event.html",
+            controller: "ShowDisplayController"
+        }).
+        otherwise ({
+            redirectTo: '/DisplayEvent'
+        });
+    });
+    app.controller("AddEventController", function($scope) {
 
-		$scope.Topics =
-		{
-			option1 : "Controller",
-			option2 : "Module"
-		};     });
+        $scope.message = "This is to Add a new Event";
+
+    });
+    app.controller("ShowDisplayController",function($scope){
+
+        $scope.message = "This is display an Event";
+
+    });
