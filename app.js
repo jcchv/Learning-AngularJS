63774f94ef136c4db09f785b2787df172fc03838
Juan <juan.chavez@autozone.com>
2018-08-21 15:17:57 -0600

    Learning Angular: Filter Examples

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-var app = angular.module('sampleApp',["ngRoute"]);
-    app.config(function($routeProvider){
-        $routeProvider.
-        when("/NewEvent",{
-            templateUrl : "add_event.html",
-            controller: "AddEventController"
-        }).
-        when("/DisplayEvent", {
-            templateUrl: "show_event.html",
-            controller: "ShowDisplayController"
-        }).
-        otherwise ({
-            redirectTo: '/DisplayEvent'
-        });
-    });
-    app.controller("AddEventController", function($scope) {
+var app = angular.module('DemoApp',[]);
+    app.controller('DemoController',function($scope){
 
-        $scope.message = "This is to Add a new Event";
-
-    });
-    app.controller("ShowDisplayController",function($scope){
-
-        $scope.message = "This is display an Event";
-
-    });
-
-		app.controller('AngularController',function($scope){
-        $scope.a=1;
-        $scope.b=1;
-
-        $scope.value=$scope.$eval('a+b');
+        $scope.tutorialName ="Angular JS";
+				$scope.tutorialID =3.565656;
+				 $scope.tutorialprice =20.56;
+				 $scope.tutorial ={TutorialID:12,tutorialName:"Angular"};
     });
